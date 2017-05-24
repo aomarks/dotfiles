8912d2736761a1d99d6868abb5b019665edeb973
Alexander Marks <aomarks@gmail.com>
2017-05-23 17:13:52 -0700

    Reorg bash files a bit.

--- macos/macos.sh
+++ macos/macos.sh
@@ -1,10 +1,6 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
 export YELLOW="\[\033[0;33m\]"
 export CLEAR="\[\033[0m\]"
 
-source $HOME/homebrew/etc/bash_completion.d/git-prompt.sh
 export GIT_PS1_SHOWCOLORHINTS=1
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
@@ -18,9 +14,6 @@ export CLICOLOR=1
 
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-
-export GOPATH=$HOME/git/go
-export PATH=$HOME/homebrew/bin:$PATH:/usr/local/opt/go/libexec/bin:$GOPATH/bin
 
 nvm-update() {
   local="$(nvm version node)"
@@ -45,5 +38,3 @@ update-all() {
   echo "> vim"
   vim +PluginUpdate +qall
 }
-
-source ~/git/dotfiles/.bashrc
